05b1f2d22ddacc15ed637d61d9271a7f3eb6d80c
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-05-31 17:40:52 +0300

    Create 05.MathOperations.js

---
+++ 01.Syntax, Functions and Statements/Lab/05.MathOperations.js
@@ -0,0 +1,29 @@
+function performMathOperation(num1, num2, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+        default:
+            console.log(`Invalid operator: ${operator}`);
+            return;
+    }
+
+    console.log(result);
+}
