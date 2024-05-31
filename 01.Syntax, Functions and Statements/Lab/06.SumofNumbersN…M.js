64566cb300e58a64d6465eee02d942371a62ebb7
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-05-31 17:43:43 +0300

    Create 06.SumofNumbersN…M.js

---
+++ 01.Syntax, Functions and Statements/Lab/06.SumofNumbersN…M.js
@@ -0,0 +1,14 @@
+function sumNumbers(n, m) {
+   
+    n = Number(n);
+    m = Number(m);
+
+    let sum = 0;
+
+    for (let i = n; i <= m; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+    
+}
