0a27af2d71568fdce134e3ca00f3cc3329c80fa3
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-06-02 00:46:32 +0300

    Update 01.Fruit.js

--- 01.Syntax, Functions and Statements/Exercise/01.Fruit.js
+++ 01.Syntax, Functions and Statements/Exercise/01.Fruit.js
@@ -2,7 +2,6 @@ function calculateFruitCost(fruit, weightInGrams, pricePerKilogram) {
 
     let weightInKilograms = weightInGrams / 1000;
 
-
     let totalCost = weightInKilograms * pricePerKilogram;
 
     weightInKilograms = weightInKilograms.toFixed(2);
