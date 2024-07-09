15692d7ae95f9d02b9910b5fb1d75ddf478f4e8b
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-07-09 15:26:22 +0300

    Create 02.PrinteveryN-thElementfromanArray.js

---
+++ 02.Arrays and Nested Arrays/Exercise/02.PrinteveryN-thElementfromanArray.js
@@ -0,0 +1,9 @@
+function getEveryNthElement(array, step) {
+    let result = [];
+    
+    for (let i = 0; i < array.length; i += step) {
+        result.push(array[i]);
+    }
+    
+    return result;
+}
